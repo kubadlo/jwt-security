395c6328be02d8bbdfdf4b4ccd5a8f7be236339a
Jakub Leško <jakub.lesko@outlook.com>
2019-01-05 12:35:06 +0100

    Initial commit

---
+++ src/main/java/com/jakublesko/jwtsecurity/SpringSecurityJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jakublesko.jwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtExampleApplication.class, args);
+    }
+}
+
